5860dcee58fea03e752df30c97ec57311b67317c
Moshik Alper <moshikalper@gmail.com>
2025-02-06 17:50:04 +0200

    Deploy frontend build to backend

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,4 @@
 export default {
   dbURL: process.env.DB_URL,
   dbName: process.env.DB_NAME,
-
-  // dbURL: 'mongodb+srv://team:Mszueq2PSuzrZVCd@cluster0.ktgte.mongodb.net/',
-  // dbName: 'board_db',
-};
+}
--- server.js
+++ server.js
@@ -27,7 +27,9 @@ if (process.env.NODE_ENV === 'production') {
         origin: [   'http://127.0.0.1:3000',
                     'http://localhost:3000',
                     'http://127.0.0.1:5173',
-                    'http://localhost:5173'
+                    'http://localhost:5173',
+                    'http://127.0.0.1:5154',
+                    'http://localhost:5154',
                 ],
         credentials: true
     }
